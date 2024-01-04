b3173c9b09651665a8825c1e67bf563aae86fa6c
D0lph1n25 <d0lph1n.25_119@outlook.jp>
2024-01-04 23:34:36 +0900

    seichis.index.htmlから座標取得

---
+++ app/javascript/mapragistration.js
@@ -0,0 +1,47 @@
+let map;
+let markers = [];
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("mapragistration"), {
+        center: { lat:  34.7024, lng: 135.4959 },
+        zoom: 13,
+    });
+
+    // クリックしたときのイベントを追加
+    map.addListener('click', function(event) {
+        placeMarker(event.latLng);
+    });
+}
+
+
+function placeMarker(location) {
+    // 既存のマーカーを削除
+    clearMarkers();
+
+    // 新しいマーカーを作成
+    const marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+
+    // マーカーを配列に追加
+    markers.push(marker);
+
+    const lat = location.lat();
+    const lng = location.lng();
+
+    console.log(`Latitude: ${lat}, Longitude: ${lng}`);
+
+    // ここで座標をサーバーに送信するなどの処理を実行できます
+}
+
+function clearMarkers() {
+    // 既存のマーカーを地図から削除
+    markers.forEach(marker => {
+        marker.setMap(null);
+    });
+
+    // マーカー配列をクリア
+    markers = [];
+}
+
--- app/javascript/maps.js
+++ app/javascript/maps.js
@@ -4,8 +4,8 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat:  34.7024, lng: 135.4959 },
+    zoom: 15,
   });
 }
 
