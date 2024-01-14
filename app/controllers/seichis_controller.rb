b7f29dd4f626f98176a16e5c9f78b739caf3c954
D0lph1n25 <d0lph1n.25_119@outlook.jp>
2024-01-14 15:07:01 +0900

    編集ページ　作成

--- app/controllers/seichis_controller.rb
+++ app/controllers/seichis_controller.rb
@@ -1,5 +1,6 @@
 class SeichisController < ApplicationController
-  before_action :move_session, only: :new
+  before_action :move_session, only: [:new, :edit]
+  before_action :seichi_find, only: [:show, :edit, :update]
 
   def index
     @seichis = Seichi.all
@@ -20,7 +21,20 @@ class SeichisController < ApplicationController
   end
 
   def show
-    @seichi = Seichi.find(params[:id])
+  end
+
+  def edit
+    if current_user.id != @seichi.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @seichi.update(seichi_params)
+      redirect_to seichi_path(@seichi.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
@@ -33,6 +47,10 @@ class SeichisController < ApplicationController
     unless user_signed_in?
       redirect_to new_user_session_path
     end
+  end
+
+  def seichi_find
+    @seichi = Seichi.find(params[:id])
   end
 
 end
