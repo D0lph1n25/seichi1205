a8f547848f30c52b74a945508af381e047210982
D0lph1n25 <d0lph1n.25_119@outlook.jp>
2024-01-18 17:10:03 +0900

    コメント削除機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,15 @@ class CommentsController < ApplicationController
     redirect_to seichi_path(comment.seichi_id)
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    seichi_id = @comment.seichi_id
+    if user_signed_in? && current_user.id == @comment.user_id
+      @comment.destroy
+    end
+    redirect_to seichi_path(seichi_id)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, seichi_id: params[:seichi_id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "seichis#index"
   resources :seichis do
-    resources :comments, only: :create
+    resources :comments, only: [:create, :destroy]
     collection do
       get 'search'
     end
