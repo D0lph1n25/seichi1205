1edd1999109ef4c6d5597da81899377fe2058298
D0lph1n25 <d0lph1n.25_119@outlook.jp>
2024-01-22 15:15:58 +0900

    お気に入り機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @nickname = current_user.nickname
     @seichis = current_user.seichis
-    @favorite_seichis = @user.favorite_seichis
+    @favorites = current_user.favorites
   end
 
 end
