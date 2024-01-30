f94ed74d7db94903a0365d9a40306f08312dfbb9
D0lph1n25 <d0lph1n.25_119@outlook.jp>
2024-01-30 23:46:58 +0900

    エラーメッセージ　日本語表示

--- app/models/seichi.rb
+++ app/models/seichi.rb
@@ -9,10 +9,12 @@ class Seichi < ApplicationRecord
 
   validates :title, presence: true
   validates :introduction, presence: true
-  validates :category_id, presence: true
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, numericality: { other_than: 1 , message: "を選択してください"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "を選択してください"}
   validates :addresses, presence: true
   validates :image, presence: true
+  validates :latitude,  numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90, message: "地図で地点を選択してください" }
+  validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180, message: "地図で地点を選択してください" }
 
   def self.ransackable_attributes(auth_oject = nil)
     ["title", "introduction", "category_id", "prefecture_id"]
