b4ba9608078915ec9e455b87767a908876dd845d
D0lph1n25 <d0lph1n.25_119@outlook.jp>
2024-02-02 23:53:32 +0900

    seichi　単体テスト

--- spec/models/seichi_spec.rb
+++ spec/models/seichi_spec.rb
@@ -20,20 +20,44 @@ RSpec.describe Seichi, type: :model do
         expect(@seichi.errors.full_messages).to include("作品名を入力してください")
       end
       it '場所の紹介が空では投稿できない' do
+        @seichi.introduction = ""
+        @seichi.valid?
+        expect(@seichi.errors.full_messages).to include("場所の紹介を入力してください")
       end
       it 'カテゴリーが「---」を選択されている（category_idが1）と投稿できない' do
+        @seichi.category_id = "1"
+        @seichi.valid?
+        expect(@seichi.errors.full_messages).to include("カテゴリーを選択してください")
       end
       it '都道府県が「---」を選択されている（prefecture_idが1）と投稿できない' do
+        @seichi.prefecture_id = "1"
+        @seichi.valid?
+        expect(@seichi.errors.full_messages).to include("都道府県を選択してください")
       end
       it '都道府県以下の住所が空では投稿できない' do
+        @seichi.addresses = ""
+        @seichi.valid?
+        expect(@seichi.errors.full_messages).to include("都道府県以下の住所を入力してください")
       end
       it '地図で地点をクリックされていないと投稿できない(latitudeが空)' do
+        @seichi.latitude = ""
+        @seichi.valid?
+        expect(@seichi.errors.full_messages).to include("地図で地点を選択してください")
       end
       it '地図で地点をクリックされていないと投稿できない(longitudeが空)' do
-      end
-      it '地図で地点をクリックされていないと投稿できない(latitude及びlongitudeが空)' do
+        @seichi.longitude = ""
+        @seichi.valid?
+        expect(@seichi.errors.full_messages).to include("地図で地点を選択してください")
       end
       it '写真が選択されていないと投稿できない' do
+        @seichi.image = nil
+        @seichi.valid?
+        expect(@seichi.errors.full_messages).to include("写真を入力してください")
+      end
+      it 'userが紐づいていないと投稿できない' do
+        @seichi.user = nil
+        @seichi.valid?
+        expect(@seichi.errors.full_messages).to include("Userを入力してください")
       end
     end
   end
