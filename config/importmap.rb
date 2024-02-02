a9d3e6ae9db3bc171924aea49c47dabc286a006c
D0lph1n25 <d0lph1n.25_119@outlook.jp>
2024-02-02 23:25:08 +0900

    地図機能修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,3 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "maps", to: "maps.js"
-pin "mapragistration", to: "mapragistration.js"
-pin "mapragistrationedit", to: "mapragistrationedit.js"
-pin "mapshow", to: "mapshow.js"
