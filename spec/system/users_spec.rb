403db4ca8fc0da5b343d721706f359f16bac86fb
D0lph1n25 <d0lph1n.25_119@outlook.jp>
2024-02-04 23:33:27 +0900

    user 結合テスト

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+def basic_pass(path)
+  username = ENV["BASIC_AUTH_USER"]
+  password = ENV["BASIC_AUTH_PASSWORD"]
+  visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
+end
+
+RSpec.describe "Users", type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  context 'ユーザー新規登録ができるとき' do
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+      # トップページに移動する
+      basic_pass root_path
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'ニックネーム', with: @user.nickname
+      fill_in 'Eメール', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      fill_in 'もう一度パスワードを入力してください', with: @user.password_confirmation
+      fill_in 'last-name', with: @user.last_name
+      fill_in 'first-name', with: @user.first_name
+      fill_in 'last-name-kana', with: @user.last_name_kana
+      fill_in 'first-name-kana', with: @user.first_name_kana
+      select '1980', from: 'user_birthday_1i'
+      select '1', from: 'user_birthday_2i'
+      select '15', from: 'user_birthday_3i'
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+        sleep 1
+      }.to change { User.count}.by(1)
+      # トップページへ遷移することを確認する
+      expect(page).to have_current_path(root_path)
+      # サインアップページへ遷移するボタンやログインページへ遷移するボタンが標示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+      # ログアウトボタンが表示されていることを確認する
+      expect(page).to have_content('ログアウト')
+      expect(page).to have_content('マイページ')
+    end
+  end
+
+end
