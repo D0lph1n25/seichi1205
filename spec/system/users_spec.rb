47615add94843a8953cd782f58df75fbd8c1e221
D0lph1n25 <d0lph1n.25_119@outlook.jp>
2024-02-04 23:48:26 +0900

    user 結合テスト　追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -47,5 +47,33 @@ RSpec.describe "Users", type: :system do
       expect(page).to have_content('マイページ')
     end
   end
+  context 'ユーザー新規登録ができないとき' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページに戻ってくる' do
+      # トップページに移動する
+      basic_pass root_path
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'ニックネーム', with: ''
+      fill_in 'Eメール', with: ''
+      fill_in 'パスワード', with: ''
+      fill_in 'もう一度パスワードを入力してください', with: ''
+      fill_in 'last-name', with: ''
+      fill_in 'first-name', with: ''
+      fill_in 'last-name-kana', with: ''
+      fill_in 'first-name-kana', with: ''
+
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+        sleep 1
+      }.to change{ User.count }.by(0)
+      # 新規登録ページへ戻されることを確認する
+      expect(page).to have_current_path(new_user_registration_path)
+    end
+  end
 
 end
